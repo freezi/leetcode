e16d8771504ff493e6aad50cb4849e8dfaad36d2
kyler <100742516+ky-ler@users.noreply.github.com>
2022-12-10 15:55:13 -0500

    Time: 97 ms (85.29%), Space: 52.1 MB (11.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = prices[0];
+    let curMax = prices[0];
     let max = 0;
     
-    for (let i = 1; i < prices.length; i++) {
-        if (min > prices[i]) min = prices[i];
-        max = Math.max(max, prices[i] - min);
+    for (const n of prices) {
+        if (n > curMax) max = Math.max(max, n - curMax);
+        else curMax = n;
     }
     
     return max;
