cfabe7d6202bd854327ddebb4f1c7f8c110bb0f0
kyler <100742516+ky-ler@users.noreply.github.com>
2022-12-10 17:47:45 -0500

    Time: 98 ms (100.00%), Space: 42.4 MB (100.00%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.js
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maximumValue = function(strs) {
-    let max = 0
+    let max = 0;
     
     for (const str of strs) {
-        if (isNaN(str)) max = Math.max(max, str.length)
+        if (isNaN(str)) max = Math.max(max, str.length);
         else {
-            max = Math.max(max, str)
+            max = Math.max(max, str);
         }
         
     }
     
-    return max
+    return max;
     
     // Shoutout to sendao
     // return Math.max(...strs.map( (x) => isNaN(x) ? x.length : x ));
