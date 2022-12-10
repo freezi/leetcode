bbb112d76e66c65f905d6b44dcee73a698561b9e
kyler <100742516+ky-ler@users.noreply.github.com>
2022-12-10 18:14:05 -0500

    Time: 62 ms (97.88%), Space: 45 MB (54.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0
-    let right = nums.length - 1
+    let left = 0;
+    let right = nums.length - 1;
     
     for (let i = 0; i < nums.length; i++) {
-        let mid = Math.floor((left + right) / 2)
+        let mid = Math.floor((left + right) / 2);
         
-        if (nums[mid] === target) return mid
-        else if (nums[mid] > target) right = mid - 1
-        else left = mid + 1
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] > target) right = mid - 1;
+        else left = mid + 1;
     }
     
-    return -1
+    return -1;
 };
