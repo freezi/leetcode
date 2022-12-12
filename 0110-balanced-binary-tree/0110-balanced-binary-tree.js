5c7c74e5fa13d212440085cb9b7013e1cc2756ec
kyler <100742516+ky-ler@users.noreply.github.com>
2022-12-12 18:10:18 -0500

    Time: 85 ms (84.33%), Space: 48.3 MB (19.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,16 +11,28 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    if (!root) return true
-    
-    const dfs = node => {
-        if (!node) return 0
-        let l = 1 + dfs(node.left)
-        let r = 1 + dfs(node.right)
+    // NeetCode
+    // https://youtu.be/QfJsau0ItOY
+    const dfs = root => {
+        if (!root) return [true, 0]
         
-        if (Math.abs(l - r) > 1) return Infinity
-        return Math.max(l, r)
+        const left = dfs(root.left)
+        const right = dfs(root.right)
+        const balanced = left[0] && right[0] && (Math.abs(left[1] - right[1]) <= 1)
+        
+        return [balanced, 1 + Math.max(left[1], right[1])]
     }
     
-    return dfs(root) !== Infinity
+    return dfs(root)[0]
+//     if (!root) return true
+    
+//     const dfs = node => {
+//         if (!node) return 0
+//         let l = 1 + dfs(node.left)
+//         let r = 1 + dfs(node.right)
+        
+//         if (Math.abs(l - r) > 1) return Infinity
+//         return Math.max(l, r)
+//     }
+//     return dfs(root) !== Infinity
 };
