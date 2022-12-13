175d372df50c4b2a0d5ba8417a3fca79e82cbe69
kyler <100742516+ky-ler@users.noreply.github.com>
2022-12-13 00:13:05 -0500

    Time: 70 ms (74.90%), Space: 14.7 MB (37.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,28 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower().split(' ')
-        newStr = ''
+        left, right = 0, len(s) - 1
         
-        for index, word in enumerate(s):
-            for x in word:
-                if x.isalnum():
-                    newStr += x
+        while left < right:
+            
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:    
+                left += 1
+                right -= 1
+            
+        return True
         
-        return newStr == newStr[::-1] 
+        
+#         s = s.lower().split(' ')
+#         newStr = ''
+        
+#         for index, word in enumerate(s):
+#             for x in word:
+#                 if x.isalnum():
+#                     newStr += x
+        
+#         return newStr == newStr[::-1] 
