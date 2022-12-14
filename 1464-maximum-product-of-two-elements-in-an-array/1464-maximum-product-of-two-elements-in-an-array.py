4bd073ef5d221cf3e4e87775afddae3d9506d4f7
kyler <100742516+ky-ler@users.noreply.github.com>
2022-12-14 02:07:22 -0500

    Time: 61 ms (83.12%), Space: 14 MB (49.22%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -4,10 +4,5 @@ class Solution:
         
         for num in nums:
             f, s = max(num, f, s), max(s, min(f, num))
-        
+
         return (f - 1) * (s - 1)
-#         f = max(nums)
-#         nums[nums.index(f)] = 0
-#         s = max(nums)
-        
-#         return (f - 1) * (s - 1)
