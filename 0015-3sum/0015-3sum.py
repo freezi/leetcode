f99eb5bf43945deb2bd4cd14349902dc0b018634
freezi <100742516+freezi@users.noreply.github.com>
2022-12-16 20:58:19 -0500

    Time: 816 ms (86.97%), Space: 18.1 MB (42.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,7 +3,7 @@ class Solution:
         nums.sort()
         ans = []
         
-        for i in range(len(nums)):
+        for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
                 
