1557ca62bf4167bad92e1b069631bff0ebb63eab
freezi <100742516+freezi@users.noreply.github.com>
2022-12-20 22:47:35 -0500

    Time: 102 ms (5.21%), Space: 13.9 MB (74.98%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,12 +1,13 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         m = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+        l = len(s)
         
-        for i in range(0, len(s) // 2):
+        for i in range(0, l // 2):
             if s[i].lower() in m:
                 m[s[i].lower()] = m.get(s[i].lower()) + 1
                 
-        for i in range(len(s) // 2, len(s)):
+        for i in range(l // 2, l):
             if s[i].lower() in m:
                 m[s[i].lower()] =  m.get(s[i].lower()) - 1
                 
